d8cc3962364a8e2755cb8ca5c5418cde71f3da99
Cesar Yanez Fernandez <cesar@cyanezfdz.me>
2015-12-04 12:05:55 -0600

    DB updates, adding columns for Errors and Stats files.

--- db_setup.sql
+++ db_setup.sql
@@ -1,10 +1,3 @@
--- InnoDB Backend Storage Settings
-SET unique_checks=0;
-SET foreign_key_checks=1;
-
--- SQL Mode
-SET sql_mode='ALLOW_INVALID_DATES,TRADITIONAL';
-
 -- PRIVILEGES NetPivot to demonio@localhost
 REVOKE ALL PRIVILEGES ON *.* FROM 'demonio'@'localhost';
 DROP USER 'demonio'@'localhost';
@@ -23,10 +16,9 @@ CREATE TABLE IF NOT EXISTS NetPivot.users (
     type VARCHAR(45) NULL,
     max_files SMALLINT UNSIGNED NULL,
     max_conversions SMALLINT UNSIGNED NULL
-)
-ENGINE = InnoDB;
+) ENGINE = InnoDB;
 
-INSERT INTO users(id,name,password,type,max_files,max_conversions) VALUES (1,'admin','$2y$10$XEAw/cVMGTy4H8flaMjpLesrkZVlRo1ZVC0fm6FjHlGTWul5vh2Ae','Administrator',100,100);
+INSERT INTO NetPivot.users(id,name,password,type,max_files,max_conversions) VALUES (1,'admin','$2y$10$XEAw/cVMGTy4H8flaMjpLesrkZVlRo1ZVC0fm6FjHlGTWul5vh2Ae','Administrator',100,100);
 
 -- TABLE NetPivot.files
 DROP TABLE IF EXISTS NetPivot.files CASCADE;
@@ -39,8 +31,7 @@ CREATE TABLE IF NOT EXISTS NetPivot.files (
 	FOREIGN KEY(users_id) REFERENCES NetPivot.users(id)
 	ON DELETE CASCADE
 	ON UPDATE NO ACTION
-)
-ENGINE = InnoDB;
+) ENGINE = InnoDB;
 
 -- TABLE NetPivot.conversions
 DROP TABLE IF EXISTS NetPivot.conversions CASCADE;
@@ -50,6 +41,8 @@ CREATE TABLE IF NOT EXISTS NetPivot.conversions (
     time_conversion DATETIME(0) NOT NULL,
     files_uuid VARCHAR(36) NOT NULL,
     converted_file VARCHAR(255) NOT NULL,
+    error_file VARCHAR(255) NULL,
+    stats_file VARCHAR(255) NULL,
     CONSTRAINT fk_conversions_users1
 	FOREIGN KEY(users_id) REFERENCES NetPivot.users(id)
 	ON DELETE CASCADE
@@ -58,8 +51,7 @@ CREATE TABLE IF NOT EXISTS NetPivot.conversions (
 	FOREIGN KEY(files_uuid) REFERENCES NetPivot.files(uuid)
 	ON DELETE NO ACTION
 	ON UPDATE NO ACTION
-)
-ENGINE = InnoDB;
+) ENGINE = InnoDB;
 
 -- TABLE NetPivot.settings
 DROP TABLE IF EXISTS NetPivot.settings CASCADE;
@@ -67,8 +59,7 @@ CREATE TABLE IF NOT EXISTS NetPivot.settings (
     host_name TINYINT UNSIGNED NOT NULL PRIMARY KEY,
     timezone VARCHAR(45) NULL,
     files_path VARCHAR(255) NULL
-)
-ENGINE = InnoDB;
+) ENGINE = InnoDB;
 
 -- PRIVILEGES NetPivot to demonio@localhost
 CREATE USER 'demonio'@'localhost' IDENTIFIED BY 'password';
