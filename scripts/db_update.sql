fd126baadc030ae319d7d296be970fccf9cb605d
Cesar Yanez Fernandez <cesar@cyanezfdz.me>
2016-01-06 07:15:39 -0600

    CHORE: change admin uname / pword at DB level.

--- scripts/db_update.sql
+++ scripts/db_update.sql
@@ -1,12 +1,10 @@
 -- This UPDATES the Layout of an already deployed DB with Data.
 -- USE WITH CARE, please backup DB before running this.
 
-LOCK TABLES
-    NetPivot.details WRITE;
+-- LOCK TABLES
+--    NetPivot.details WRITE;
 
-ALTER TABLE NetPivot.details
-    MODIFY obj_name VARCHAR(160) NULL AFTER obj_component,
-    MODIFY attribute VARCHAR(128) NULL AFTER obj_name;
+UPDATE NetPivot.users SET password = '$2y$10$G.TH1hSw9wQcQOTqZjIJNudYm1jfQIjxFthJBnbJhmSTJQrpiU2la' WHERE id = '1';
 
-UNLOCK TABLES;
+-- UNLOCK TABLES;
 
