5f23837c532b72e1a8a8b8cad496c12047d48495
Cesar Yanez Fernandez <cesar@cyanezfdz.me>
2016-01-07 09:26:53 -0600

    Fixing a condition in codedeploy ApplicationStop cycle.

--- scripts/appstop.sh
+++ scripts/appstop.sh
@@ -2,13 +2,16 @@
 
 clean() {
     local WWWDATA=/var/www/html
-    local BACKUP=/home/ubuntu/netpivot/netpivot-${DEPLOYMENT_ID}.txz
+    if [ -n "$DEPLOYMENT_ID" ]; then
+	local BACKUP=/home/ubuntu/netpivot/netpivot-${DEPLOYMENT_ID}.txz
+    else
+	local BACKUP=/home/ubuntu/netpivot/netpivot-BACKUP.txz
+    fi
     local FILELIST=( html php png css map woff2 eot svg ttf woff js )
     local DIRLIST=( `find ${WWWDATA} -type d` )
 
-    if [ ! -f ${BACKUP} ]; then
-	tar -cJf ${BACKUP} -C ${WWWDATA} .
-    fi
+    rm -f ${BACKUP}
+    tar -cJf ${BACKUP} -C ${WWWDATA} .
 
     if [ ! -f ${WWWDATA}/.keep ]; then
 	touch ${WWWDATA}/.keep
