d6fce3dfdf965c992007a8778da5b021c93534d6
Cesar Yanez Fernandez <cesar@cyanezfdz.me>
2016-01-07 10:57:55 -0600

    Adding more echoes to CodeDeploy and fixing backup names.

--- scripts/after.sh
+++ scripts/after.sh
@@ -2,9 +2,9 @@
 
 LAST=`find /opt/netpivot -name "f5conv.*" -exec basename {} \; | sort | tail -n 1`
 
-find /opt/netpivot -name "f5conv" -type l -exec rm -f {} \;
-find /opt/netpivot -name "f5conv.*" -type f -exec chown www-data.www-data {} \;
-find /opt/netpivot -name "f5conv.*" -type f -exec chmod 0755 {} \;
+find /opt/netpivot -name "f5conv" -type l -exec rm -vf {} \;
+find /opt/netpivot -name "f5conv.*" -type f -exec chown -c www-data.www-data {} \;
+find /opt/netpivot -name "f5conv.*" -type f -exec chmod -c 0755 {} \;
 
-ln -snf ${LAST} /opt/netpivot/f5conv
+ln -snvf ${LAST} /opt/netpivot/f5conv
 
--- scripts/appstop.sh
+++ scripts/appstop.sh
@@ -2,15 +2,10 @@
 
 clean() {
     local WWWDATA=/var/www/html
-    if [ -n "$DEPLOYMENT_ID" ]; then
-	local BACKUP=/home/ubuntu/netpivot/netpivot-${DEPLOYMENT_ID}.txz
-    else
-	local BACKUP=/home/ubuntu/netpivot/netpivot-BACKUP.txz
-    fi
+    local BACKUP=/home/ubuntu/netpivot/netpivot-`date "+%F_%H-%M-%S_%Z"`.txz
     local FILELIST=( html php png css map woff2 eot svg ttf woff js )
     local DIRLIST=( `find ${WWWDATA} -type d` )
 
-    rm -vf ${BACKUP}
     tar -cvJf ${BACKUP} -C ${WWWDATA} .
 
     if [ ! -f ${WWWDATA}/.keep ]; then
@@ -18,7 +13,7 @@ clean() {
     fi
 
     for file in ${FILELIST[*]}; do
-	find ${WWWDATA} -name "*.${file}" -exec rm -vf {} \;
+	find ${WWWDATA} -name "*.${file}" -type f -exec rm -vf {} \;
     done
 
     for dir in ${DIRLIST[*]}; do
