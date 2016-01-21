9769685da4150143f5bdedd0ed8392c354d84fcf
Cesar Yanez Fernandez <cesar@cyanezfdz.me>
2016-01-21 10:54:04 -0600

    DB layout cleanup

--- scripts/db_update.sql
+++ scripts/db_update.sql
@@ -1,10 +1,41 @@
 -- This UPDATES the Layout of an already deployed DB with Data.
 -- USE WITH CARE, please backup DB before running this.
 
--- LOCK TABLES
---    NetPivot.details WRITE;
+DROP TABLE IF EXISTS NetPivot.settings CASCADE;
+DROP TABLE IF EXISTS NetPivot.stats CASCADE;
 
-UPDATE NetPivot.users SET password = '$2y$10$G.TH1hSw9wQcQOTqZjIJNudYm1jfQIjxFthJBnbJhmSTJQrpiU2la' WHERE id = '1';
+LOCK TABLES
+    NetPivot.files WRITE,
+    NetPivot.conversions WRITE,
+    NetPivot.details WRITE;
 
--- UNLOCK TABLES;
+ALTER TABLE NetPivot.conversions
+    DROP FOREIGN KEY fk_conversions_files1
+;
+
+ALTER TABLE NetPivot.details
+    DROP FOREIGN KEY fk_details_files1
+;
+
+ALTER TABLE NetPivot.files
+    MODIFY uuid CHAR(36) NOT NULL FIRST
+;
+
+ALTER TABLE NetPivot.conversions
+    MODIFY files_uuid CHAR(36) NOT NULL AFTER time_conversion,
+    ADD CONSTRAINT fk_conversions_files1
+        FOREIGN KEY(files_uuid) REFERENCES NetPivot.files(uuid)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+;
+
+ALTER TABLE NetPivot.details
+    MODIFY files_uuid CHAR(36) NOT NULL FIRST,
+    ADD CONSTRAINT fk_details_files1
+        FOREIGN KEY(files_uuid) REFERENCES NetPivot.files(uuid)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+;
+
+UNLOCK TABLES;
 
