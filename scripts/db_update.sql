1866fdab5f8d93f3bdb958aacd906f4bfb676167
Cesar Yanez Fernandez <cesar@cyanezfdz.me>
2016-02-22 16:07:43 -0600

    Workaround: There is an error at convertion Action because of timezone not found.

--- scripts/db_update.sql
+++ scripts/db_update.sql
@@ -1,41 +1,15 @@
 -- This UPDATES the Layout of an already deployed DB with Data.
 -- USE WITH CARE, please backup DB before running this.
 
-DROP TABLE IF EXISTS NetPivot.settings CASCADE;
-DROP TABLE IF EXISTS NetPivot.stats CASCADE;
+-- LOCK TABLES
+--    NetPivot.details WRITE;
+-- UNLOCK TABLES;
 
-LOCK TABLES
-    NetPivot.files WRITE,
-    NetPivot.conversions WRITE,
-    NetPivot.details WRITE;
+CREATE TABLE IF NOT EXISTS NetPivot.settings (
+    host_name TINYINT UNSIGNED NOT NULL PRIMARY KEY,
+    timezone VARCHAR(45) NOT NULL DEFAULT 'US/Eastern',
+    files_path VARCHAR(255) NULL
+) ENGINE = InnoDB;
 
-ALTER TABLE NetPivot.conversions
-    DROP FOREIGN KEY fk_conversions_files1
-;
-
-ALTER TABLE NetPivot.details
-    DROP FOREIGN KEY fk_details_files1
-;
-
-ALTER TABLE NetPivot.files
-    MODIFY uuid CHAR(36) NOT NULL FIRST
-;
-
-ALTER TABLE NetPivot.conversions
-    MODIFY files_uuid CHAR(36) NOT NULL AFTER time_conversion,
-    ADD CONSTRAINT fk_conversions_files1
-        FOREIGN KEY(files_uuid) REFERENCES NetPivot.files(uuid)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
-;
-
-ALTER TABLE NetPivot.details
-    MODIFY files_uuid CHAR(36) NOT NULL FIRST,
-    ADD CONSTRAINT fk_details_files1
-        FOREIGN KEY(files_uuid) REFERENCES NetPivot.files(uuid)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
-;
-
-UNLOCK TABLES;
+REPLACE INTO NetPivot.settings(host_name, timezone, files_path) VALUES (1, 'US/Eastern', '/var/www/html/dashboard/files');
 
