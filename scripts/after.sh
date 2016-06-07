61424ac6d3313dcd02ca30df7d7d523ae67c668b
Cesar Yanez Fernandez <cesar@cyanezfdz.me>
2016-06-07 11:23:58 -0500

    Round 4.

--- scripts/after.sh
+++ scripts/after.sh
@@ -8,3 +8,8 @@
 
 # ln -snvf ${LAST} /opt/netpivot/f5conv
 
+if [ -f /var/www/html/dashboard/f5conv ]; then
+    chown -c www-data.www-data /var/www/html/dashboard/f5conv
+    chmod -c 0755 /var/www/html/dashboard/f5conv
+fi
+
