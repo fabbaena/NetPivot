4cb4f9d7fe7d484a7e1a9bce73ed7f7688e54e58
Cesar Yanez Fernandez <cesar@cyanezfdz.me>
2016-06-27 15:39:31 -0500

    Restoring because CodeDeploy is bad.

--- scripts/after.sh
+++ scripts/after.sh
@@ -7,3 +7,10 @@
 # find /opt/netpivot -name "f5conv.*" -type f -exec chmod -c 0755 {} \;
 
 # ln -snvf ${LAST} /opt/netpivot/f5conv
+
+if [ ! -f /var/www/html/dashboard/f5conv ]; then
+    cp -v /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/netpivot_kernel/f5conv /var/www/html/dashboard/f5conv
+    chown -c www-data.www-data /var/www/html/dashboard/f5conv
+    chmod -c 0755 /var/www/html/dashboard/f5conv
+fi
+
