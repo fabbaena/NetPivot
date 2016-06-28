4c5fbb4e97312d4c0bcccfc0cf25e8c02978f593
Fabian Baena <fabian.baena@samanagroup.co>
2016-06-28 17:55:27 -0400

    fix codedeploy issues

--- scripts/3-before.sh
+++ scripts/3-before.sh
@@ -15,12 +15,8 @@ create() {
 alter() {
     local DBALTER=/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts/pgsql_update.sql
     #local DBALTER=/home/ubuntu/codedeploy/scripts/db_update.sql
-    export PGHOST=localhost
-    export PGUSER=demonio
-    export PGPASSWORD=s3cur3s0c
-    export PGDATABASE=netpivot
 
-    psql -b -f ${DBALTER}
+    psql -U demonio-b -f ${DBALTER} netpivot
 }
 
 invoke-rc.d --quiet postgresql status
