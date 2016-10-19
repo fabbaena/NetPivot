c17f86a54bfea1a9308c521eae457a6aa1145234
Fabian Baena <fabian.baena@samanagroup.co>
2016-10-19 07:25:08 -0400

    version 1.1.0
    * viewport for mobile device moved to css.php

--- content/index.php
+++ content/index.php
@@ -29,7 +29,6 @@ if(isset($user)) {
             $(".register").click(function() { document.location="register.php";});
             })
         </script>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body>
         <link href="css/signing.css" rel="stylesheet">
