b9f41b2379e1a309526bb8840728c5e95428bf23
Fabian Baena <fabian.baena@samanagroup.co>
2017-04-13 01:04:28 -0400

    version 1.1.9
    * fixing bug at file list for older versions

--- content/js/fileupload.js
+++ content/js/fileupload.js
@@ -277,7 +277,9 @@ function showFilelist(data) {
 
 function version_alert(data) {
 
-	if(typeof data._conversion !== "undefined" && typeof data._conversion.np_version !== "undefined") {
+	if(typeof data._conversion !== "undefined" && 
+		data._conversion !== null && 
+		typeof data._conversion.np_version !== "undefined") {
 		v = data._conversion.np_version;
 		var file_ver = v.split(".");
 		var np = np_version.split(".");
