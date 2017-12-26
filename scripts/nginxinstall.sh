e66e1b0de235e5dd7f6a717910dd3188d00f2709
Fabian Baena <fabian.baena@samanagroup.co>
2017-12-26 17:56:24 -0500

    added script to install nginx

--- scripts/nginxinstall.sh
+++ scripts/nginxinstall.sh
@@ -8,12 +8,13 @@ sudo chown ubuntu.ubuntu /opt/netpivot/usr/share
 cd /opt/netpivot/usr/share/
 git clone git@gitlab.com:nemacrux/samana-prototype.git nginx
 if [ -d "nginx" ]; then
-    cd nginx
-    git clone https://github.com/fatiherikli/nginxparser
-    sudo python3 nginxparser/setup.py install
     sudo apt-get install python3-pip
+    cd nginx
     sudo pip3 install -r requirements.pip
     git clone -b python3-update https://github.com/netscalerconfig/netscaler
+    git clone https://github.com/fatiherikli/nginxparser
+    cd nginxparser
+    sudo python3 nginxparser/setup.py install
 else
     echo "Unable to download nginx"
 fi
