caa3ad3c521b67fe56df56ea52a293e4a45185f0
THPdmontes <niner90@ciencias.unam.mx>
2018-11-08 05:44:19 -0600

    Secures list_files end-point.

--- content/engine/rest_list_files.php
+++ content/engine/rest_list_files.php
@@ -1,37 +1,45 @@
 <?php
+require_once dirname(__FILE__) .'/../model/FileManager.php';
+require_once dirname(__FILE__) .'/../model/Conversions.php';
+require_once dirname(__FILE__) .'/../model/AuthJwt.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+function getListFiles($user_id){
+    $file_list = new FileList($records=['users_id' => $user_id]);
 
-require_once '../model/FileManager.php';
-require_once '../model/Conversions.php';
+    if($file_list->count > 0){
+        $files = $file_list->files;
 
-$users_id = '1';
+        $out = [];
+        foreach($files as $file){
+            $current = [
+                'id' => $file->uuid,
+                'filename' => $file->filename,
+                'upload_time' => $file->upload_time,
+                'size' => $file->size
+            ];
 
-$file_list = new FileList($records=['users_id' => $users_id]);
+            $conv = new Conversion(['files_uuid' => $file->uuid, 'users_id' => $user_id]);
+            $current['converted'] = $conv->load(['files_uuid', 'users_id']);
 
-if($file_list->count > 0){
-    $files = $file_list->files;
+            $out[] = $current;
+        }
 
-    $out = [];
-    foreach($files as $file){
-        $current = [
-	       'id' => $file->uuid, 
-	       'filename' => $file->filename,
-	       'upload_time' => $file->upload_time,
-	       'size' => $file->size
-	];
-	
-	$conv = new Conversion(['files_uuid' => $file->uuid, 'users_id' => $users_id]);
-	$current['converted'] = $conv->load(['files_uuid', 'users_id']);
-
-	$out[] = $current;
+        return json_encode($out);
+    } else{
+        return json_encode(
+            ['message' => 'There are no converions for this user.']
+        );
     }
+}
 
-    echo json_encode($out);
-} else{
-    echo json_encode(
-        ['message' => 'There are no converions for this user.']
-    );
-};
+$jwt = AuthJwt::getTokenFromHeaders(getallheaders());
+$auth = new AuthJwt();
+if ($auth->validJwt($jwt)){
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    $user_id = $auth->getUserId($jwt);
+    echo getListFiles($user_id);
 
+}else{
+    header("HTTP/1.1 401 Unauthorized");
+}
