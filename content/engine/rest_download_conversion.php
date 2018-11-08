2f68dbb038781da9bae23761338119928acc49cd
THPdmontes <niner90@ciencias.unam.mx>
2018-11-08 08:42:30 -0600

    Secures `download_conversion` end-point.

--- content/engine/rest_download_conversion.php
+++ content/engine/rest_download_conversion.php
@@ -1,30 +1,36 @@
 <?php
+require_once dirname(__FILE__) .'/../model/AuthJwt.php';
+require_once dirname(__FILE__) .'/../model/Conversions.php';
 
-header('Access-Control-Allow-Origin: *');
+$jwt = AuthJwt::getTokenFromHeaders(getallheaders());
+$auth = new AuthJwt();
+if ($auth->validJwt($jwt)){
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,
+        Access-Control-Allow-Origin, Content-Type,
+        Access-Control-Allow-Methods');
 
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, 
-    Access-Control-Allow-Origin, Content-Type, 
-    Access-Control-Allow-Methods');
+    $users_id = $auth->getUserId($jwt);
 
-require_once '../model/Conversions.php';
+    // Obtain incoming request
+    $incoming = json_decode(file_get_contents("php://input"));
 
-$users_id = 1;
+    $uuid = $incoming->id;
 
-// Obtain incoming request
-$incoming = json_decode(file_get_contents("php://input"));
+    $conv = new Conversion(['files_uuid' => $uuid, 'users_id' => $users_id]);
 
-$uuid = $incoming->id;
+    if($conv->load(['files_uuid', 'users_id'])){
+        header('Content-Type: text/plain');
+        readfile($conv->converted_file);
 
-$conv = new Conversion(['files_uuid' => $uuid, 'users_id' => $users_id]);
+    } else{
+        header('Content-Type: application/json');
+        echo json_encode([
+            'message' => 'No such conversion.'
+        ]);
+    }
 
-if($conv->load(['files_uuid', 'users_id'])){
-    header('Content-Type: text/plain');
-    readfile($conv->converted_file);
-
-} else{
-    header('Content-Type: application/json');
-    echo json_encode([
-        'message' => 'No such conversion.'
-    ]);
+}else{
+    header("HTTP/1.1 401 Unauthorized");
 }
