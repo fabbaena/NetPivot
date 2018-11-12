59b64b734a9407f4a81c6b34c65877b5561301c8
Fabian Baena <fabian.baena@samanagroup.co>
2018-11-12 17:19:32 -0500

    fix warning about header with multiple lines

--- content/engine/rest_stats_conversion.php
+++ content/engine/rest_stats_conversion.php
@@ -19,9 +19,7 @@ if ($auth->validJwt($jwt)){
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,
-        Access-Control-Allow-Origin, Content-Type,
-        Access-Control-Allow-Methods');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Origin, Content-Type, Access-Control-Allow-Methods');
 
     $users_id = $auth->getUserId($jwt);
     // Obtain incoming request
