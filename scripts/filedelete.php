468e6f2ae83ccd881bae6c3b7a8bb5c7fee89c5e
Fabian Baena <fabbaena@gmail.com>
2023-01-18 14:03:39 -0500

    cleanup scripts added

---
+++ scripts/filedelete.php
@@ -0,0 +1,42 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once '../content/model/FileManager.php';
+
+if(!isset($argv[1])) {
+    print("Error - UUID of the conversion is needed.\n");
+    exit();
+}
+$uuid=$argv[1];
+
+$output = null;
+$retval = null;
+exec('id -u', $output, $retval);
+if($output[0] != "0") {
+    print("Error - This command must be run as root.\n");
+    exit(1);
+}
+
+$file = new FileManager(array('uuid' => $uuid));
+$file->load('uuid');
+
+print("Ready to delete $uuid.\n");
+$file->toString();
+
+if(!$file->delete()) {
+    print("Error - Unable to delete database records for $uuid.\n");
+    exit(1);
+}
+$allfiles = glob("/var/www/files/$uuid*", 0);
+foreach($allfiles as $file) {
+    $status=unlink($file);
+    if(!$status){
+        print("Error - Unable to delete file $file.\n");
+    }
+}
+
+?>
---
+++ scripts/filelist.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../content/model/FileManager.php';
+
+if(!isset($argv[1])) {
+    print("Error - define a start date.\n");
+    return 1;
+}
+$filelist = new FileList(array('condition'=>array("upload_time", "<", $argv[1])));
+foreach($filelist->files as $f) {
+    print("$f->uuid\n");
+}
+
+?>
